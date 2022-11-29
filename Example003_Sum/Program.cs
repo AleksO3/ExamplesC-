4ec62342d7fde56a3e4b28b3b81488d87dd57bc9
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-11-29 06:09:32 +0400

    new Random().Next(min,max)

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,9 @@
-int A = 33;
+/*int A = 33;
 int B =7;
-Console.WriteLine(A + B);
+Console.WriteLine(A + B);*/
+
+int A = new Random().Next(0,1000);
+Console.WriteLine(A);
+int b = new Random().Next(0,1000);
+Console.WriteLine(b);
+Console.WriteLine(A + b);
